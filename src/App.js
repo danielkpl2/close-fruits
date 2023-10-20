7e9b0ee7424f8e58bf032d5b322145869131fcec
Daniel Kasprowicz <danielkpl2@gmail.com>
2023-10-20 17:22:23 +0100

    adding the solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState, memo } from 'react';
 
-function App() {
+
+
+// Implement a feature to allow item selection with the following requirements:
+// 1. Clicking an item selects/unselects it.
+// 2. Multiple items can be selected at a time.
+// 3. Make sure to avoid unnecessary re-renders of each list item in the big list (performance).
+// 4. Currently selected items should be visually highlighted.
+// 5. Currently selected items' names should be shown at the top of the page.
+//
+// Feel free to change the component structure at will.
+
+
+const App = ({ items }) => {
+  const [selectedItems, setSelectedItems] = useState(new Set());
+	  
+  let handleItemClick = (item) => {
+    if(selectedItems.has(item)){
+      const updatedSelectedItems = new Set(selectedItems);
+      updatedSelectedItems.delete(item);
+      setSelectedItems(updatedSelectedItems);
+    } else {
+	    const updatedSelectedItems = new Set(selectedItems);
+      updatedSelectedItems.add(item);
+      setSelectedItems(updatedSelectedItems);
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <Fragment>
+    <div><span>{Array.from(selectedItems).join(', ')}</span></div>
+    <ul className="List">
+      {items.map(item => (
+        <MemoizedListItem
+            key={item.name}
+            item={item}
+            isSelected={selectedItems.has(item.name)}
+            handleItemClick={handleItemClick}
+          />
+      ))}
+    </ul>
+  </Fragment>)
+};
 
-export default App;
+const ListItem = ({ item, isSelected, handleItemClick }) => (
+  <li
+    onClick={() => handleItemClick(item.name)}
+    className={`List__item List__item--${item.color} ${isSelected && 'selected'}`}
+  >
+    {item.name}
+  </li>
+);
+
+const MemoizedListItem = memo(ListItem);
+
+export default App
