87aafb01434ed9cc93893bc6d960d76cce47977d
Daniel Kasprowicz <danielkpl2@gmail.com>
2023-10-20 17:23:39 +0100

    reformatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { Fragment, useState, memo } from 'react';
-
-
+import { Fragment, useState, memo } from "react";
 
 // Implement a feature to allow item selection with the following requirements:
 // 1. Clicking an item selects/unselects it.
@@ -11,42 +9,46 @@ import { Fragment, useState, memo } from 'react';
 //
 // Feel free to change the component structure at will.
 
-
 const App = ({ items }) => {
   const [selectedItems, setSelectedItems] = useState(new Set());
-	  
+
   let handleItemClick = (item) => {
-    if(selectedItems.has(item)){
+    if (selectedItems.has(item)) {
       const updatedSelectedItems = new Set(selectedItems);
       updatedSelectedItems.delete(item);
       setSelectedItems(updatedSelectedItems);
     } else {
-	    const updatedSelectedItems = new Set(selectedItems);
+      const updatedSelectedItems = new Set(selectedItems);
       updatedSelectedItems.add(item);
       setSelectedItems(updatedSelectedItems);
     }
-  }
-  
+  };
+
   return (
-  <Fragment>
-    <div><span>{Array.from(selectedItems).join(', ')}</span></div>
-    <ul className="List">
-      {items.map(item => (
-        <MemoizedListItem
+    <Fragment>
+      <div>
+        <span>{Array.from(selectedItems).join(", ")}</span>
+      </div>
+      <ul className="List">
+        {items.map((item) => (
+          <MemoizedListItem
             key={item.name}
             item={item}
             isSelected={selectedItems.has(item.name)}
             handleItemClick={handleItemClick}
           />
-      ))}
-    </ul>
-  </Fragment>)
+        ))}
+      </ul>
+    </Fragment>
+  );
 };
 
 const ListItem = ({ item, isSelected, handleItemClick }) => (
   <li
     onClick={() => handleItemClick(item.name)}
-    className={`List__item List__item--${item.color} ${isSelected && 'selected'}`}
+    className={`List__item List__item--${item.color} ${
+      isSelected && "selected"
+    }`}
   >
     {item.name}
   </li>
@@ -54,4 +56,4 @@ const ListItem = ({ item, isSelected, handleItemClick }) => (
 
 const MemoizedListItem = memo(ListItem);
 
-export default App
+export default App;
